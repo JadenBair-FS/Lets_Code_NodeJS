eb3e6ca5f51edf45c3b07c19f838cd8816affb6d
Jaden Bair <jmbair@student.fullsail.edu>
2023-08-05 13:36:35 -0400

    Added router, request listener and both fullfilled design pattern S/L/R

---
+++ app/app.js
@@ -0,0 +1,36 @@
+const router = require("../router/router");
+
+const express = require("express");
+const app = express();
+
+// use middleware to parse json payLoads into our request model
+app.use(express.json());
+// req.body.username req.body.password
+
+// service actuator - localhost:3000
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Service is up" });
+});
+
+// use middleware to define my router
+app.use("/exercise", router);
+
+// use middelware to handle errors and bad url paths
+
+app.use((req,res,next) => {
+    const error = new Error("Not Found");
+    error.status=404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status,
+        }
+    })
+})
+
+module.exports = app;
